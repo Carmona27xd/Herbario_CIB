fe0b3b9bae195ebb1bc496d4bdfd949b56dab345
Maltteada <Soyunpokemonytuno@hotmail.com>
2025-04-09 13:12:49 -0600

    "Integracion"

--- database/connectionDB.php
+++ database/connectionDB.php
@@ -3,13 +3,13 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 $host = "localhost";  
-$usuario = "root";    
+$username = "root";    
 $password = "FinalFantasy_7";       
-$base_datos = "herbarium_cib";  
+$dbname = "herbarium_cib";  
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$base_datos", $usuario, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Error: " . $e->getMessage());
 }

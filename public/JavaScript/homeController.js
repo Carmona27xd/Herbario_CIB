b339d291bb27cd8eb7e57e17c1d45ebc6f23678b
Carmona <lecape27@gmail.com>
2025-04-20 18:08:57 -0600

    Cambios realizados

---
+++ public/JavaScript/homeController.js
@@ -0,0 +1,8 @@
+document.getElementById("buttonLogIn").addEventListener("click", function() {
+    window.location.href = "logIn.html";
+})
+
+document.getElementById("buttonColector").addEventListener("click", function() {
+    window.location.href = "colectorRequest.html";
+})
+

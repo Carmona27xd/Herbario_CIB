3f2692af2f08c8b4fa0aef6b25097f07d8f7162d
Maltteada <Soyunpokemonytuno@hotmail.com>
2025-06-03 23:36:58 -0600

    Arreglo de problemas

--- public/JavaScript/homeController.js
+++ public/JavaScript/homeController.js
@@ -1,8 +1,16 @@
-document.getElementById("buttonLogIn").addEventListener("click", function() {
-    window.location.href = "logIn.html";
-})
+document.addEventListener("DOMContentLoaded", function () {
+  const loginButton = document.getElementById("buttonLogIn");
+  if (loginButton) {
+    loginButton.addEventListener("click", function () {
+      window.location.href = "logIn.html";
+    });
+  }
 
-document.getElementById("buttonColector").addEventListener("click", function() {
-    window.location.href = "collectorApplication.html";
-})
+  const collectorButton = document.getElementById("buttonColector");
+  if (collectorButton) {
+    collectorButton.addEventListener("click", function () {
+      window.location.href = "collectorApplication.html";
+    });
+  }
+});
 
